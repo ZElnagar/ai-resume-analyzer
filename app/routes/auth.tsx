fcd28fdb5fd05b421f2f12a32b07171ecea25040
Ziad Elnagar <zelnagar@uwo.ca>
2025-08-21 02:17:59 -0400

    Done basic homepage, need to work on auth

---
+++ app/routes/auth.tsx
@@ -0,0 +1,14 @@
+export const meta = () => ([
+    {title: 'Resumind | Auth'},
+    {name: 'description', content: 'Log into your account'}
+])
+
+const Auth = () => {
+    return (
+        <div>
+            auth
+        </div>
+    );
+};
+
+export default Auth;
