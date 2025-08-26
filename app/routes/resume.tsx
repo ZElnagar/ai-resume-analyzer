2d42d496b12d670cd13f33d3cef68e692750095a
Ziad Elnagar <zelnagar@uwo.ca>
2025-08-26 00:51:00 -0400

    'Cloud storage, PDF-Image conversion, Key-Value storage and AI feedback Completed'

---
+++ app/routes/resume.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useParams} from "react-router";
+
+const Resume = () => {
+    const { id } = useParams();
+
+    return (
+        <div>Resume {id}</div>
+    );
+};
+
+export default Resume;
